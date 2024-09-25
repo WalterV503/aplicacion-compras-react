96c08b6b57138573b51623004745f996134806ea
WalterV503 <waltercasquez@gmail.com>
2024-09-25 09:56:08 -0600

    Se ha creado una aplicación de carrito de compras con react-vite

---
+++ src/CarritoCompras.jsx
@@ -0,0 +1,26 @@
+import { Navigate, Route, Routes } from "react-router-dom"
+import { NavBar } from "./components/NavBar"
+import { CompraPage } from "./pages/CompraPage"
+import { CarritoPage } from "./pages/CarritoPage"
+import { ProductosProvider } from "./context/ProductosProvider"
+import { CarritoProvider } from "./context/CarritoProvider"
+
+export const CarritoCompras = () => {
+  return (
+    <>
+      <ProductosProvider>
+        <CarritoProvider>
+          <NavBar>
+          </NavBar>
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<CompraPage></CompraPage>}></Route>
+              <Route path="/carrito" element={<CarritoPage></CarritoPage>}></Route>
+              <Route path="/*" element={<Navigate to="/"></Navigate>}></Route>
+            </Routes>
+          </div>
+        </CarritoProvider>
+      </ProductosProvider>
+    </>
+  )
+}
---
+++ src/context/ProductosProvider.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { ProductosContext } from "./ProductosContex"
+
+export const ProductosProvider = ({children}) => {
+
+    const [productos, setProductos] = useState([])
+
+    const fetchProductos = async() =>{
+        const response = await fetch('https://fakestoreapi.com/products')
+        const data = await response.json()
+        setProductos(data)
+        console.log(data)
+    }
+
+    useEffect(() => {
+      fetchProductos()
+    }, [])
+
+  return (
+    <ProductosContext.Provider value={{productos}}>
+        {children}
+    </ProductosContext.Provider>
+  )
+}
